51b12c56162fe3c7401fa8ea27843ddca03ccb1d
kiars1 <DeadHaze317@yandex.ru>
2023-04-01 16:28:53 +0700

    submit fix

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let button = document.querySelector(".button");
 let closebutton = document.querySelector(".popup-button");
 let popup = document.querySelector(".popup");
 let video = document.querySelector(".video");
-var ytplayer;
+let input= document.querySelector("input");
 
-document.addEventListener("keydown", keySearch);
-button.addEventListener("click", search);
-
-function keySearch(e) {
-    if (e.code == 'Enter') {
-        search();
-    }
+let listener = async event => {
+    event.preventDefault();
+    search();
 }
+
+document.addEventListener("submit", listener);
+
+button.addEventListener("click", search);
 
 function keyClose(e) {
     if (e.code == 'Escape') {
@@ -22,24 +22,25 @@ function keyClose(e) {
 function search() {
     let Input = document.querySelector(".input");
     if (Input.value.length <= 0) {
-    video.setAttribute('src', 'https://www.youtube.com/embed/GtL1huin9EE');
+        video.setAttribute('src', 'https://www.youtube.com/embed/GtL1huin9EE');
+        input.toggleAttribute("disabled", true);
         popup.classList.add('popup-oppen');
         document.addEventListener("keydown", keyClose);
         closebutton.addEventListener("click", close);
-        document.removeEventListener("keydown", keySearch);
         button.removeEventListener("click", search);
-        player.addEventListener("keydown", keyClose);
+        document.removeEventListener("submit", listener);
     } else {
     window.open(["https://yandex.ru/search/?text=" + Input.value], "_self");
     }
 }
 
 function close() {
+    input.toggleAttribute("disabled", false);
+    input.focus();
     popup.classList.remove('popup-oppen');
     video.setAttribute('src', ' ');
     document.removeEventListener("keydown", keyClose);
     closebutton.removeEventListener("click", close);
-    document.addEventListener("keydown", keySearch);
-    document.addEventListener("keydown", keySearch);
     button.addEventListener("click", search);
+    document.addEventListener("submit", listener);
 }
