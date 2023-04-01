8a4c932822ec0b17182ff38874c13aecce03b929
kiars1 <DeadHaze317@yandex.ru>
2023-04-01 22:44:27 +0700

    add popup and rickroll

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let button = document.querySelector(".button");
 let closebutton = document.querySelector(".popup-button");
 let popup = document.querySelector(".popup");
-let video = document.querySelector(".video");
-let input= document.querySelector("input");
+let input = document.querySelector("input");
 
 let listener = async event => {
     event.preventDefault();
@@ -22,15 +21,16 @@ function keyClose(e) {
 function search() {
     let Input = document.querySelector(".input");
     if (Input.value.length <= 0) {
-        video.setAttribute('src', 'https://www.youtube.com/embed/GtL1huin9EE');
         input.toggleAttribute("disabled", true);
         popup.classList.add('popup-oppen');
         document.addEventListener("keydown", keyClose);
         closebutton.addEventListener("click", close);
         button.removeEventListener("click", search);
         document.removeEventListener("submit", listener);
+    } else if (input.value.toLowerCase() === 'rickroll' || input.value.toLowerCase() === 'рикролл') {
+        window.open(['https://www.youtube.com/watch?v=dQw4w9WgXcQ'], "_self");
     } else {
-    window.open(["https://yandex.ru/search/?text=" + Input.value], "_self");
+        window.open(["https://yandex.ru/search/?text=" + Input.value], "_self");
     }
 }
 
@@ -38,7 +38,6 @@ function close() {
     input.toggleAttribute("disabled", false);
     input.focus();
     popup.classList.remove('popup-oppen');
-    video.setAttribute('src', ' ');
     document.removeEventListener("keydown", keyClose);
     closebutton.removeEventListener("click", close);
     button.addEventListener("click", search);
